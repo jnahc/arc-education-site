ee59010e22f5bf41b9fb73dd3108e81ffc828e99
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-06 12:02:41 -0800

    set up models

--- arc_profile/models.py
+++ arc_profile/models.py
@@ -15,16 +15,7 @@ class Profile(models.Model):
   def __str__(self):
     return self.name
 
-class Course(models.Model):
-  title = models.CharField(max_length=100)
-  description = models.TextField(default="")
-  start_date = models.DateField()
-  end_date = models.DateField()
-  category = models.CharField(max_length=100)
-  profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='courses')
 
-def __str__(self):
-  return f"{self.title}"
 
   
 
