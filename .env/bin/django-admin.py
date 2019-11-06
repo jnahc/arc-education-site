3411e28d98e554dd5a01984a524c399410bd31dd
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-06 11:09:51 -0800

    initial commit

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jeffchan/sei/deliverables/academy-revolutionary-confederation/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
