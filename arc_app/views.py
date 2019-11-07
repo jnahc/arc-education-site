6f498b2f45582482335de5249935e7398d8625a0
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-07 15:25:31 -0800

    views updated

--- arc_app/views.py
+++ arc_app/views.py
@@ -6,21 +6,16 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-
 from .models import Course
 from .forms import CourseForm
 
-
-
 # Create your views here.
-
 
 def home(request):
   return render(request, 'index.html')
 
 def about_us(request):
   return render(request, 'about_us.html')
-
 
 def course_create(request,):
   if request.method == 'POST':
@@ -33,7 +28,6 @@ def course_create(request,):
   context = {'from': form}
   return render(request, 'course_form.html', context)
 
-
 def course_list(request):
       return render(request, 'course_list.html')
     
@@ -42,190 +36,12 @@ def course_detail(request, pk):
   context = {"course": course}
   return render(request, "course_detail.html", context)
 
-
-
-
 def api_courses(request):
   all_courses = Course.objects.all()
   data = []
   for course in all_courses:
     data.append({"title": course.title, "description": course.description, "start_date": course.start_date, "end_date": course.end_date})
   return JsonResponse({"data": data, "status": 200})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def course_create(request):
   if request.method == 'POST':
