9d0f783347558b1b082f2468facfd57c59ed5c29
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-07 17:06:00 -0800

    remove default value from course model

--- arc_app/models.py
+++ arc_app/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
   description = models.TextField(default="")
   start_date = models.DateField()
   end_date = models.DateField()
-  photo_url = models.TextField(default="https://previews.123rf.com/images/jevee/jevee1606/jevee160600132/58016549-pastel-drawn-textured-background-in-blue-colors-blank-for-letter-or-greeting-card-a4-size-format-ser.jpg")
+  photo_url = models.TextField(default="")
   category = models.CharField(max_length=20)
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
 
