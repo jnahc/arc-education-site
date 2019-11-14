b7d98a545a5380b42e515bee61c434ead3e7c13c
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-13 21:29:34 -0800

    heroku deployment continued

--- arc/settings.py
+++ arc/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
