907a31d4ea83f50a2bfd3b0b8a1e26945de8b8c3
jeff-chan-sf-sei-05 <emailjeffchan@gmail.com>
2019-11-13 21:33:00 -0800

    attempting heroku again

--- arc/settings.py
+++ arc/settings.py
@@ -126,7 +126,7 @@ django_heroku.settings(locals())
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
